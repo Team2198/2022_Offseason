79b26b8fe102b99ece644169e8f758f2f083db26
unknown <darren.chung2@student.tdsb.on.ca>
2022-02-13 20:57:00 -0500

    Changes to elevator code

--- src/main/java/frc/robot/subsystems/elevator.java
+++ src/main/java/frc/robot/subsystems/elevator.java
@@ -6,11 +6,19 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.XboxController;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+ 
+
+
 /** Add your docs here. */
-public class elevator {
+public class Elevator extends SubsystemBase {
+  
     WPI_TalonSRX EOne;
+    XboxController xboxController;   
+    GenericHID genericHID;
     
-    if (xboxController.getBumper(GenericHID.HandRight)== true){
+    public  Elevator(){
+     if (xboxController.getRightBumperPressed()== true){
         EOne.set(0.5);
     }
     else {
@@ -18,4 +26,5 @@ public class elevator {
     }
 }
 }
+
 
